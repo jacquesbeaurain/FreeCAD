cc051b2a19793f874d22c092fd6b437083258866
Jacques Beaurain <jacques.beaurain@gmail.com>
2026-06-12 12:31:37 -0700

    Revert "tmp"
    This reverts commit ba6dd33fc1b8c0e2f6f1d49c71d6554e0631325c.

--- src/Mod/Sketcher/Gui/ViewProviderSketch.cpp
+++ src/Mod/Sketcher/Gui/ViewProviderSketch.cpp
@@ -96,6 +96,7 @@
 #include <TopExp.hxx>
 #include <TopTools_IndexedDataMapOfShapeListOfShape.hxx>
 #include <TopTools_IndexedMapOfShape.hxx>
+#include <TopTools_ListIteratorOfListOfShape.hxx>
 
 #include <Mod/Part/Gui/SoFCShapeObject.h>
 
