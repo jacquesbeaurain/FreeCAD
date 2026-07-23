e5f7406e51b885faa3d413d059c5b01aed05af14
Jacques Beaurain <jacques.beaurain@gmail.com>
2026-07-23 10:57:38 -0700

    tmp fix for OCC 8.0 deadlock (issue FreeCAD#31457)

--- src/Mod/Part/App/TopoShapeExpansion.cpp
+++ src/Mod/Part/App/TopoShapeExpansion.cpp
@@ -4971,11 +4971,7 @@ TopoShape& TopoShape::makeElementFace(
             mkFace->addTopoShape(shape);
         }
     }
-#if OCC_VERSION_HEX >= 0x070600
-    mkFace->Build(std::make_unique<Part::ProgressIndicator>()->Start());
-#else
     mkFace->Build();
-#endif
 
     const auto& ret = mkFace->getTopoShape();
     setShape(ret._Shape);
@@ -6225,11 +6221,7 @@ TopoShape& TopoShape::makeElementBoolean(
     else if (tolerance < 0.0) {
         FCBRepAlgoAPIHelper::setAutoFuzzy(mk.get());
     }
-#if OCC_VERSION_HEX >= 0x070600
-    mk->Build(std::make_unique<Part::ProgressIndicator>()->Start());
-#else
     mk->Build();
-#endif
     if (Base::Sequencer().wasCanceled()) {
         FC_THROWM(Base::CADKernelError, "User aborted");
     }
